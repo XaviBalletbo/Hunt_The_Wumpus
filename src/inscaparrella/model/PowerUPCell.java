f61e259791970ba238ab5f29fdbfceb6dd746560
XaviBalletbo <xballetbo@inscaparrella.cat>
2025-03-20 15:51:15 +0100

    Martin maricon

--- src/inscaparrella/model/PowerUPCell.java
+++ src/inscaparrella/model/PowerUPCell.java
@@ -4,6 +4,8 @@ import inscaparrella.utils.CellType;
 import inscaparrella.utils.InhabitantType;
 import inscaparrella.utils.PowerUp;
 
+import javax.swing.*;
+
 public class PowerUPCell extends Cell{
 
    PowerUp power;
@@ -14,22 +16,53 @@ public class PowerUPCell extends Cell{
     }
     public PowerUPCell(int row, int col){
         super(row, col, CellType.POWERUP, false);
-        this.power = PowerUp.values((int) Math.random(*3));
+        this.power = PowerUp.values()[(int) Math.random()*3];
     }
 
     public PowerUPCell(int row, int col, boolean open, PowerUp power){
-        super(row, col,CellType.POWERUP, false);
+        super(row, col,CellType.POWERUP, open);
+        this.power = power;
+    }
 
+    private void createPowerUPCell(){
+        power = PowerUp.values()[(int) Math.random()*3];
+    }
+
+    public PowerUp consumePowerUp(){
+        PowerUp power1;
+        if(isOpen()==true){
+            power1 = power;
+            power = PowerUp.NONE;
+        }else{
+            power1 = PowerUp.NONE;
+        }
+        return power1;
     }
 
 
     @Override
     public String emitEcho() {
-        return "";
+        String str = "";
+        if (power == PowerUp.NONE ){
+            str = "";
+        }else str= "Clic, clic...";
+        return str;
     }
 
     @Override
     public boolean isDangerus() {
         return false;
+    }
+
+    @Override
+    public String toString(){
+        String str= "";
+        if (power == PowerUp.ARROW){
+            str= "Cel·la ["+row+", " +col+ "] -Tipus POWERUP (Concedeix el poder de ARROW)";
+        }else if (power == PowerUp.JUMPER_BOOTS){
+            str = "Cel·la ["+row+ ", "+col+ "] -Tipus POWERUP (Concedeix el poder JUMPER BOOTS)";
+        }
+        else str = "Cel·la ["+row+ ", "+col+ "] -Tipus POWERUP";
+        return str;
     }
 }
